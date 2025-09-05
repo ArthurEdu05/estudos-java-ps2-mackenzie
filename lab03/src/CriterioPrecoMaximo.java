297378ef49890ca3951ff18a95dde365b1c60cde
Arthur Eduardo <163680207+ArthurEdu05@users.noreply.github.com>
2025-09-05 12:00:19 +0000

    feat(lab03): interface implementation study

---
+++ lab03/src/CriterioPrecoMaximo.java
@@ -0,0 +1,10 @@
+public class CriterioPrecoMaximo implements CriterioBusca{
+    public boolean testar(Produto p, String valor) {
+        if(p.getPreco() <= Double.parseDouble(valor)){
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+}
